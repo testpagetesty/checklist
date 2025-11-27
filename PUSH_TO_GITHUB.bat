6fdfca06897f560c656ec0ad15cdcb6f55248848
testpagetesty <arsenrenok31@gmail.com>
2025-11-27 10:42:22 +0300

    Удалены файлы с токеном GitHub для безопасности

--- PUSH_TO_GITHUB.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Отправка проекта на GitHub...
-echo.
-git push https://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@github.com/testpagetesty/checklist.git main
-echo.
-echo Если появилась ошибка "Repository not found", создайте репозиторий на https://github.com/new
-echo Имя репозитория: checklist
-pause
-
